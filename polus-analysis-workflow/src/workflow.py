6230e65c5b51c94a194ad78c2c8836109b4ab845
EC2 Default User <ec2-user@ip-172-31-71-81.ec2.internal>
2022-05-19 13:41:17 +0000

    updating input argument arguments for segmentation plugins

--- polus-analysis-workflow/src/workflow.py
+++ polus-analysis-workflow/src/workflow.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.INFO)
 
 
 def create_output_folder(outDir, pluginName) -> pathlib.Path:
-    outname = '-'.join(pluginName.split()[0:-1]) + '-' + 'outputs'
+    outname =  "-".join(pluginName.split()) +'_' +'outputs'
     outpath = Path(outDir, outname)
     if not outpath.exists():
         os.makedirs(Path(outpath))
@@ -45,7 +45,8 @@ def Run_FlatField_Correction(inpDir:pathlib.Path,
     pl.outDir=outpath
     if not dryrun:
         pl.run(gpus=None)
-    return outpath
+    newpath = Path(outpath, 'images')
+    return newpath
 
 def ApplyFlatfield(inpDir:pathlib.Path,
                             filePattern:str,
@@ -61,7 +62,7 @@ def ApplyFlatfield(inpDir:pathlib.Path,
     pl.ffDir= ffDir
     pl.brightPattern = 'p01_x(01-24)_y(01-16)_wx(0-2)_wy(0-2)_c{c}_flatfield.ome.tif'
     pl.darkPattern = 'p01_x(01-24)_y(01-16)_wx(0-2)_wy(0-2)_c{c}_darkfield.ome.tif'
-    pl.photoPattern = ' '
+    pl.photoPattern = ''
     outpath = create_output_folder(outDir, pluginName)
     pl.outDir=outpath
     if not dryrun:
@@ -73,15 +74,16 @@ def Run_Montage(inpDir:pathlib.Path,
                 outDir:pathlib.Path,
                 dryrun:bool=True
                 ) -> None:
-    url='https://raw.githubusercontent.com/Nicholas-Schaub/polus-plugins/fix/montage/transforms/images/polus-montage-plugin/plugin.json'
+    # url='https://raw.githubusercontent.com/Nicholas-Schaub/polus-plugins/fix/montage/transforms/images/polus-montage-plugin/plugin.json'
+    url=Path('/home/ec2-user/data/plugin-manifest/montage.json')
     polus.plugins.submit_plugin(url, refresh=True)
-    pl = plugins.Montage
+    pl = plugins.Montage2
     pluginName = pl.name
     pl.inpDir=inpDir
     pl.filePattern=filePattern
-    pl.layout='tp', 'xy'
-    pl.imageSpacing='1'
-    pl.gridSpacing='20'
+    pl.layout = 'tp, xy'
+    pl.imageSpacing=1
+    pl.gridSpacing=20
     pl.flipAxis='p'
     outpath = create_output_folder(outDir, pluginName)
     pl.outDir=outpath
@@ -89,15 +91,12 @@ def Run_Montage(inpDir:pathlib.Path,
         pl.run(gpus=None)
     return outpath
 
-def Recycle_Vector(inpDir:pathlib.Path, stitchDir:pathlib.Path, groupBy:str, filePattern:str, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+def Recycle_Vector(inpDir:pathlib.Path, stitchDir:pathlib.Path, filePattern:str, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
     pl = plugins.RecycleStitchingVectorPlugin
     pluginName = pl.name
-    pl.stitchRegex=filePattern
-    pl.collectionRegex=filePattern
+    pl.filepattern=filePattern
     pl.stitchDir=stitchDir
     pl.collectionDir=inpDir
-    # groupBy='xytp'
-    pl.groupBy=groupBy
     outpath = create_output_folder(outDir, pluginName)
     pl.outDir=outpath
     if not dryrun:
@@ -105,6 +104,8 @@ def Recycle_Vector(inpDir:pathlib.Path, stitchDir:pathlib.Path, groupBy:str, fil
     return outpath
 
 def Image_Assembler(inpDir:pathlib.Path, stitchPath:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+    url='https://raw.githubusercontent.com/PolusAI/polus-plugins/dev/transforms/images/polus-image-assembler-plugin/plugin.json'
+    polus.plugins.submit_plugin(url, refresh=True)
     pl = plugins.ImageAssembler
     pluginName = pl.name
     pl.stitchPath=stitchPath
@@ -131,13 +132,13 @@ def precompute_slide(inpDir:pathlib.Path, filePattern:str, imageType:str, outDir
     return outpath
 
 def SplineDist(inpDir:pathlib.Path, filePattern:str, modelDir:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
-    url = Path('/home/ec2-user/data/manifest/splinedist.json')
+    url = Path('/home/ec2-user/data/manifests/splinedist.json')
     polus.plugins.submit_plugin(url, refresh=True)
     pl = plugins.SplinedistInferencePlugin
     pluginName = pl.name
     pl.inpImageDir=inpDir
     pl.inpBaseDir=modelDir
-    imagePattern=filePattern
+    pl.imagePattern=filePattern
     outpath = create_output_folder(outDir, pluginName)
     pl.outDir=outpath
     if not dryrun:
@@ -161,13 +162,14 @@ def ImagenetModelFeaturization(inpDir:pathlib.Path, model:str, resolution:str, o
     return outpath
 
 
-def cellposeInference(inpDir:pathlib.Path, filePattern:str, model:str, resolution:str, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+def cellposeInference(inpDir:pathlib.Path, filePattern:str,outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
     url = 'https://raw.githubusercontent.com/nishaq503/polus-plugins/plugin/cellpose-inference/segmentation/polus-cellpose-inference-plugin/plugin.json'
     polus.plugins.submit_plugin(url, refresh=True)
     pl = plugins.CellposeInference
     pluginName = pl.name
     pl.inpDir = inpDir
     pl.filePattern = filePattern
+    pl.diameter=0
     pl.diameterMode='PixelSize'
     pl.pretrainedModel = 'nuclei'
     outpath = create_output_folder(outDir, pluginName)
@@ -176,11 +178,12 @@ def cellposeInference(inpDir:pathlib.Path, filePattern:str, model:str, resolutio
         pl.run(gpus=None)
     return outpath
 
-def SMP_training_inference(inpDir:pathlib.Path, filePattern:str, model:pathlib.Path, resolution:str, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+def SMP_training_inference(inpDir:pathlib.Path, filePattern:str, model:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
     url = 'https://raw.githubusercontent.com/nishaq503/polus-plugins/plugin/smp-training/segmentation/polus-smp-training-plugin/plugin.json'
     polus.plugins.submit_plugin(url, refresh=True)
     pl = plugins.DemoSMPTrainingInference
     pluginName = pl.name
+    pl.inferenceMode = "active"
     pl.imagesInferenceDir = inpDir
     pl.inferencePattern = filePattern
     pl.inferenceMode=True
