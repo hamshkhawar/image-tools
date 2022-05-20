e5c308b4a373360c225e10032668c27503b29ac3
EC2 Default User <ec2-user@ip-172-31-71-81.ec2.internal>
2022-05-20 19:09:32 +0000

    modifying nyxus executable and smp-training

--- polus-analysis-workflow/src/workflow.py
+++ polus-analysis-workflow/src/workflow.py
@@ -3,8 +3,9 @@ from polus.plugins import plugins
 from polus.data import collections
 from pathlib import Path
 import os
-from typing import Optional
+from typing import Optional, Dict
 import pathlib, logging
+import subprocess
 
 
 logging.basicConfig(
@@ -15,17 +16,63 @@ logger = logging.getLogger('workflow')
 logger.setLevel(logging.INFO)
 
 
-
 def create_output_folder(outDir, pluginName) -> pathlib.Path:
-    outname =  "-".join(pluginName.split()) +'_' +'outputs'
+    if 'plugin' in pluginName:
+        outname = '_'.join(pluginName.split('-')[:-1]) 
+        outname = f'{outname}_outputs'
+    else:
+        outname = f'{pluginName}_outputs'
     outpath = Path(outDir, outname)
     if not outpath.exists():
         os.makedirs(Path(outpath))
         f'{outname} directory is created'
     else:
         f'{outname} directory already exists'
-    return outpath
+    return outpath, outname
 
+def pull_docker_image(plugin_name:str, 
+                    version:str, tag:str)-> None:
+    command = ['docker', 'pull', f'{tag}/{plugin_name}:{version}'] 
+    command = " ".join(command)
+    os.system(command)
+    print(f'{plugin_name} Pulling docker image')
+
+def creating_volume(volume_name:str):
+    command = ['docker','volume','create',f'{volume_name}']
+    command = ' '.join(command)
+    os.system(command)
+    f'Creating docker volume locally: {volume_name}'
+    return
+
+def removing_volume(volume_name:str):
+    command = ['docker','volume', 'rm',f'{volume_name}']
+    command = ' '.join(command)
+    f'Removing docker volume locally: {volume_name}'
+    return os.system(command)
+
+def run_command(pluginName:str, 
+                root_dir:pathlib.Path, 
+                volume_name:str,
+                tag:str, 
+                version:str, 
+                args:Dict[str, str]
+                )-> None:
+    command = [
+                'docker',
+                'run',
+                '-v',
+                f'{root_dir}:/{volume_name}',
+                f'{tag}/{pluginName}:{version}'
+            ]
+    command.extend(f'--{i}={o}' for i, o in args.items())
+    if pluginName == 'nyxus':
+        command = ' '.join(command)
+        os.system(command)
+    else:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE)
+        stdout, sterr = p.communicate()
+
+    return
 
 def Run_FlatField_Correction(inpDir:pathlib.Path,
                             filePattern:str,
@@ -178,51 +225,103 @@ def cellposeInference(inpDir:pathlib.Path, filePattern:str,outDir:pathlib.Path, 
         pl.run(gpus=None)
     return outpath
 
-def SMP_training_inference(inpDir:pathlib.Path, filePattern:str, model:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
-    url = 'https://raw.githubusercontent.com/nishaq503/polus-plugins/plugin/smp-training/segmentation/polus-smp-training-plugin/plugin.json'
-    polus.plugins.submit_plugin(url, refresh=True)
-    pl = plugins.DemoSMPTrainingInference
-    pluginName = pl.name
-    pl.inferenceMode = "active"
-    pl.imagesInferenceDir = inpDir
-    pl.inferencePattern = filePattern
-    pl.inferenceMode=True
-    pl.pretrainedModel = model
-    outpath = create_output_folder(outDir, pluginName)
-    pl.outDir=outpath
+# def SMP_training_inference(inpDir:pathlib.Path, filePattern:str, model:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+#     url = 'https://raw.githubusercontent.com/nishaq503/polus-plugins/plugin/smp-training/segmentation/polus-smp-training-plugin/plugin.json'
+#     polus.plugins.submit_plugin(url, refresh=True)
+#     pl = plugins.DemoSMPTrainingInference
+#     pluginName = pl.name
+#     pl.inferenceMode = "active"
+#     pl.imagesInferenceDir = inpDir
+#     pl.inferencePattern = filePattern
+#     pl.inferenceMode=True
+#     pl.pretrainedModel = model
+#     outpath = create_output_folder(outDir, pluginName)
+#     pl.outDir=outpath
+#     if not dryrun:
+#         pl.run(gpus=None)
+#     return outpath
+
+
+def SMP_training_inference(inpDir:pathlib.Path, filePattern:str, modelDir:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+    pluginName="polus-smp-training-plugin"
+    version= '0.5.6'
+    tag='labshare'
+    pull_docker_image(pluginName, version, tag)
+    _, outname=create_output_folder(outDir, pluginName)
+    root_dir=os.path.split(inpDir)[0]
+    inpDir = os.path.split(inpDir)[-1]
+    model_dir = os.path.split(modelDir)[-1]
+    volume_name='data2'
+    creating_volume(volume_name)
+    args = {
+            'inferenceMode': 'active',
+            'imagesInferenceDir': f'/{volume_name}/{inpDir}',
+            'inferencePattern': filePattern,
+            'pretrainedModel': f'/{volume_name}/{model_dir}',
+            'outputDir': f'/{volume_name}/{outname}'}
+
     if not dryrun:
-        pl.run(gpus=None)
-    return outpath
+        run_command(pluginName, root_dir, volume_name, tag, version, args)
 
+    print(f'Removing docker volume after finished running docker image')
 
-def FtlLabel(inpDir:pathlib.Path, outDir:pathlib.Path, connectivity:Optional[int] = 1, binarizationThreshold:Optional[int] = 0.5, VERSION:Optional[str] = None, dryrun:bool=True):
-    url = 'https://raw.githubusercontent.com/PolusAI/polus-plugins/master/transforms/images/polus-ftl-label-plugin/plugin.json'
+    removing_volume(volume_name)
+
+    return 
+
+def FtlLabel(inpDir:pathlib.Path, outDir:pathlib.Path, VERSION:Optional[str] = None, dryrun:bool=True):
+    # url = 'https://raw.githubusercontent.com/nishaq503/polus-plugins/ftl/feat/binarization-threshold/transforms/images/polus-ftl-label-plugin/plugin.json'
+    url = Path('/home/ec2-user/anaconda3/envs/py39/lib/python3.9/site-packages/polus/manifests/labshare/FtlLabel_M0m3p11.json')
     polus.plugins.submit_plugin(url, refresh=True)
     pl = plugins.FtlLabel
     pluginName = pl.name
     pl.inpDir = inpDir
-    pl.connectivity = connectivity
-    pl.binarizationThreshold=binarizationThreshold
+    pl.connectivity = '1'
+    pl.binarizationThreshold='0.5'
     outpath = create_output_folder(outDir, pluginName)
     pl.outDir=outpath
     if not dryrun:
         pl.run(gpus=None)
-    return outpath
+    return outpath 
 
-def Nyxus(inpDir:pathlib.Path, segDir:pathlib.Path, filePattern:str, csvFile:str, outDir:pathlib.Path, features:Optional[str] = "*ALL*", VERSION:Optional[str] = None, dryrun:bool=True):
-    url = 'https://raw.githubusercontent.com/friskluft/nyxus/main/plugin.json'
-    polus.plugins.submit_plugin(url, refresh=True)
-    pl = plugins.Nyxus
-    pluginName = pl.name
-    pl.intDir = inpDir
-    pl.segDir = segDir
-    pl.filePattern = ".*"
-    pl.csvFile=csvFile
-    pl.features = features
-    outpath = create_output_folder(outDir, pluginName)
-    pl.outDir=outpath
+# def Nyxus(inpDir:pathlib.Path, segDir:pathlib.Path, filePattern:str, csvFile:str, outDir:pathlib.Path, features:Optional[str] = "*ALL*", VERSION:Optional[str] = None, dryrun:bool=True):
+#     url = 'https://raw.githubusercontent.com/friskluft/nyxus/main/plugin.json'
+#     polus.plugins.submit_plugin(url, refresh=True)
+#     pl = plugins.Nyxus
+#     pluginName = pl.name
+#     pl.intDir = inpDir
+#     pl.segDir = segDir
+#     pl.filePattern = ".*"
+#     pl.csvFile=csvFile
+#     pl.features = features
+#     outpath = create_output_folder(outDir, pluginName)
+#     pl.outDir=outpath
+#     if not dryrun:
+#         pl.run(gpus=None)
+#     return outpath
+
+
+def Nyxus_exe(inpDir:pathlib.Path, segDir:pathlib.Path, filePattern:str, outDir:pathlib.Path, dryrun:bool=True):
+    pluginName='nyxus'
+    outDir, outname=create_output_folder(outDir, pluginName)
+    filePattern='.*'
+    features="*all*"
+    csvFile="separatecsv"
+    ARGS = {
+        'intDir': inpDir,
+        'segDir': segDir,
+        'outDir': outDir,
+        'filePattern': filePattern,
+        'csvFile': csvFile,
+        'features': features
+    }
+    command = [
+                '/home/ec2-user/nyxus-20220520.linuxexe'             
+            ]
+    command.extend(f'--{i}={o}' for i, o in ARGS.items())
+    command = ' '.join(command)
     if not dryrun:
-        pl.run(gpus=None)
-    return outpath
+        os.system(command)
+    return 
 
 
